a057fb0d8e08cb1e1d76bd95148d1b68849aefca
unknown <sanchezlando@gmail.com>
2023-06-24 09:06:44 +0800

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Arrow name="sanchez" address="alba tago surigao del sur"/>
     </div>
   );
+}
+const Arrow = (props)=>{
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.address}</h2>
+
+    </div>
+
+  )
 }
 
 export default App;
